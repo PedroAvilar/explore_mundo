f118db7ba8c9ed7a32f5ef4f0371bd30f3be1440
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-23 17:59:49 -0300

    Criação da seção de botões.

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,41 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final Color color = Theme.of(context).primaryColor;
+
+    final Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'LIGAR'),
+        _buildButtonColumn(color, Icons.near_me, 'ROTA'),
+        _buildButtonColumn(color, Icons.share, 'COMPARTILHAR'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Demonstração de layout Flutter',
       home: Scaffold(
@@ -16,6 +49,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         )
       ),
