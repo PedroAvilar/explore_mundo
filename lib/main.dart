e7d383743a2d12989e14d7ad6858f172c89abbbc
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-23 22:57:50 -0300

    Alteração no main e inclusão do formulário.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 import 'widgets/avaliacao_widget.dart';
+import 'widgets/formulario_avaliacao.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Widget> _avaliacoes = [
+    const AvaliacaoWidget(
+      nomeCliente: 'Pedro Avilar',
+      estrelas: 5,
+      comentario: 'Paisagens de tirar o fôlego. Experiência inesquecível.',
+    ),
+    const AvaliacaoWidget(
+      nomeCliente: 'Claudio Pontes', 
+      estrelas: 4,
+      comentario: 'Excelente para relaxar e curtir a família, não recomendo no inverno.'
+    ),
+    const AvaliacaoWidget(
+      nomeCliente: 'Jane Anne',
+      estrelas: 3,
+      comentario: 'Muito bonito, mas o acesso é um pouco difícil.'
+    ),
+  ];
+
+  void _adicionarAvaliacao(String nome, int estrelas, String comentario) {
+      setState(() {
+        _avaliacoes.add(
+          AvaliacaoWidget(
+            nomeCliente: nome,
+            estrelas: estrelas,
+            comentario: comentario
+          ),
+        );
+      });
+    }
 
   Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
@@ -24,44 +60,6 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final Color color = Theme.of(context).primaryColor;
-
-    final Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'LIGAR'),
-        _buildButtonColumn(color, Icons.near_me, 'ROTA'),
-        _buildButtonColumn(color, Icons.share, 'COMPARTILHAR'),
-      ],
-    );
-
-    return MaterialApp(
-      title: 'Demonstração de layout Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Demonstração de layout Flutter'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'assets/imagens_lugares/oeschinen-lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-            ...avaliacoes,
-          ],
-        )
-      ),
     );
   }
 
@@ -115,20 +113,42 @@ class MyApp extends StatelessWidget {
     ),
   );
 
-  final List<Widget> avaliacoes = [
-    const AvaliacaoWidget(
-      nomeCliente: 'Pedro Avilar',
-      estrelas: 5,
-      comentario: 'Paisagens de tirar o fôlego. Experiência inesquecível.',
-    ),
-    const AvaliacaoWidget(
-      nomeCliente: 'Claudio Pontes', 
-      estrelas: 4,
-      comentario: 'Excelente para relaxar e curtir a família, não recomendo no inverno.'
-    ),
-    const AvaliacaoWidget(
-      nomeCliente: 'Jane Anne',
-      estrelas: 3,
-      comentario: 'Muito bonito, mas o acesso é um pouco difícil.')
-  ];
+  @override
+  Widget build(BuildContext context) {
+
+    final Color color = Theme.of(context).primaryColor;
+
+    final Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'LIGAR'),
+        _buildButtonColumn(color, Icons.near_me, 'ROTA'),
+        _buildButtonColumn(color, Icons.share, 'COMPARTILHAR'),
+      ],
+    );
+
+    return MaterialApp(
+      title: 'Demonstração de layout Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Demonstração de layout Flutter'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/imagens_lugares/oeschinen-lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+            FormularioAvaliacao(onEnviar: _adicionarAvaliacao),
+            ..._avaliacoes,
+          ],
+        ),
+      ),
+    );
+  }
 }
