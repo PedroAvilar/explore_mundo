f0813904cfbb2e355dafceb807de897c7b7fcc29
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-24 14:10:25 -0300

    Criação da classe de avaliação.

---
+++ model/avaliacao.dart
@@ -0,0 +1,11 @@
+class Avaliacao {
+  final String nome;
+  final int estrelas;
+  final String comentario;
+
+  Avaliacao({
+    required this.nome,
+    required this.estrelas,
+    required this.comentario,
+  });
+}
