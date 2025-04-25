167a411fae5d16bad8b831bca6a18ab8d90fbc8b
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-24 21:09:23 -0300

    Correção em destinos para uso da tela base.

--- lib/pages/destinos_page.dart
+++ lib/pages/destinos_page.dart
@@ -1,31 +1,26 @@
 import 'package:explore_mundo/model/dados_destinos.dart';
 import 'package:explore_mundo/utils/media_avaliacoes.dart';
-import 'package:explore_mundo/widgets/menu_navegacao.dart';
+import 'package:explore_mundo/widgets/tela_base.dart';
 import 'package:flutter/material.dart';
 import 'package:explore_mundo/widgets/estrela_media.dart';
 
+//Tela que lista os destinos
 class DestinosPage extends StatelessWidget {
   const DestinosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Destinos'),
-      ),
-      drawer: MenuNavegacao(
-        aoSelecionar: (rota) {
-          if (ModalRoute.of(context)?.settings.name != rota) {
-            Navigator.pushNamed(context, rota);
-          }
-        },
-      ),
-      body: ListView.builder(
+    return TelaBase(
+      //Título na AppBar
+      titulo: 'Destinos',
+      //Corpo da tela
+      corpo: ListView.builder(
         itemCount: destinos.length,
         itemBuilder: (context, index) {
           final destino = destinos[index];
           final mediaEstrelas = calcularMediaAvaliacoes(destino.avaliacoes);
 
+          //Cartão visual para cada destino
           return Card(
             margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
             elevation: 4,
@@ -33,6 +28,7 @@ class DestinosPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                //Imagem do destino
                 ClipRRect(
                   borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
                   child: Image.asset(
@@ -42,15 +38,18 @@ class DestinosPage extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                //Espaço com nome e média das estrelas do destino
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+                      //Nome do destino
                       Text(
                         destino.nome,
                         style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
                       ),
+                      //Widget que exibe a média das estrelas
                       EstrelaMedia(media: mediaEstrelas),
                     ],
                   ),
