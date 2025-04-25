fde40c86cf0d87f68f6670de559405d757b030b9
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-24 21:13:07 -0300

    Correção em home para uso da tela base.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
+import 'package:explore_mundo/widgets/tela_base.dart';
 import 'package:flutter/material.dart';
-import '../widgets/menu_navegacao.dart';
 
 //Classe da tela inicial
 class HomePage extends StatelessWidget {
@@ -8,20 +8,11 @@ class HomePage extends StatelessWidget {
   //Método que constrói a interface da HomePage
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Explore Mundo'),
-      ),
-      //Menu lateral de navegação
-      drawer: MenuNavegacao(
-        aoSelecionar: (rota) {
-          if (ModalRoute.of(context)?.settings.name != rota) {
-            Navigator.pushNamed(context, rota);
-          }
-        },
-      ),
+    return TelaBase(
+      //Título na AppBar
+      titulo: 'Explore Mundo',
       //Corpo da tela
-      body: Center(
+      corpo: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
