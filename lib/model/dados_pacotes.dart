4d90e1bae41233e3d1ccd1aa6b6b0c0469b55e55
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-04-26 21:08:27 -0300

    Alteração nas descrições dos pacotes.

--- lib/model/dados_pacotes.dart
+++ lib/model/dados_pacotes.dart
@@ -5,7 +5,21 @@ final List<PacoteViagem> listaPacotes = [
   PacoteViagem(
     id: 'pacote1',
     nome: 'Aventura nos Alpes',
-    descricao: 'Explore trilhas incríveis, lagos cristalinos os Alpes suíços.',
+    descricao: '''
+      Se você busca uma experiência única que combina aventura, tranquilidade e beleza natural, este pacote é perfeito.
+      Desfrute de cinco dias de pura magia em um dos cenários naturais mais deslumbrantes do mundo: o magnífico Oeschinen Lake.
+
+      ✅ Está incluído no pacote:
+      🏨 Hospedagem: 4 noites em um charmoso hotel alpino, com café da manhã incluso, para que você comece seus dias com energia.
+      🚠 Transporte: Transfer do aeroporto e acesso ao lago por teleférico, garantindo uma experiência prática e confortável.
+      🌿 Passeios e atrações:
+          ➤ Trilhas panorâmicas com vistas cinematográficas das montanhas.
+          ➤ Passeio de barco nas águas azul-turquesa do lago.
+          ➤ Para os aventureiros: tobogã alpino com descidas emocionantes!
+          ➤ No inverno: caminhada na neve e pesca no gelo, para explorar a natureza em sua forma mais pura.
+
+      Garanta já a sua vaga e venha viver momentos inesquecíveis! ✨
+    ''',
     destinos: ['Oeschinen Lake'],
     preco: 2499.99,
     duracaoDias: 5,
@@ -26,7 +40,20 @@ final List<PacoteViagem> listaPacotes = [
   PacoteViagem(
     id: 'pacote2',
     nome: 'Encanto das cachoeiras',
-    descricao: 'Conheça Lauterbrunnen e viva todo o encanto oferecido pelo vilarejo rodeado de cachoeiras.',
+    descricao: '''
+      Explore um dos lugares mais mágicos da Suíça, vivenciando um conto de fadas com a perfeita combinação de aventura, tranquilidade e paisagens de tirar o fôlego.
+
+      ✅ Está incluído no pacote:
+      🏨 Hospedagem: 6 noites em um charmoso hotel alpino, com café da manhã incluso, proporcionando conforto e sabor pela manhã.
+      🚆 Transporte: Transfer do aeroporto e acesso facilitado a Lauterbrunnen por trem ou carro, garantindo uma viagem tranquila.
+      🌿 Passeios e atrações:
+          ➤ Exploração das 72 cachoeiras que adornam o vale, incluindo a impressionante Staubbachfall.
+          ➤ Trilhas panorâmicas e passeios de bicicleta por paisagens incríveis.
+          ➤ Viagem até Jungfraujoch - O Topo da Europa, onde você poderá ver geleiras eternas e sentir o poder dos Alpes.
+          ➤ No inverno: Esportes na neve, incluindo esqui e snowboard para os aventureiros.
+
+      Garanta já a sua vaga e venha viver momentos inesquecíveis! ✨
+    ''',
     destinos: ['Lauterbrunnen'],
     preco: 2899.00,
     duracaoDias: 7,
@@ -47,7 +74,21 @@ final List<PacoteViagem> listaPacotes = [
   PacoteViagem(
     id: 'pacote3',
     nome: 'Cidade sem carros',
-    descricao: 'Aventura, tranquilidade e lazer em qualquer estação do ano.',
+    descricao: '''
+      Prepare-se para uma aventura inesquecível em Zermatt! Este pacote proporciona uma experiência completa no coração dos Alpes Suíços, oferecendo paisagens de tirar o fôlego, tranquilidade e atrações únicas.
+      Conheça a cidade livre de carros, oferecendo um ambiente tranquilo e ideal para explorar a pé ou utilizando transportes elétricos.
+
+      ✅ Está incluído no pacote:
+      🏨 Hospedagem: 10 noites em acomodações aconchegantes e bem localizadas, com café da manhã incluso.
+      🚆 Transporte: Transfers do aeroporto e transporte local para facilitar seus deslocamentos em Zermatt.
+      🌿 Passeios e atrações:
+          ➤ Visita ao Matterhorn Glacier Paradise – o ponto mais alto da Europa acessível por teleférico.
+          ➤ Passeio pela Gornergrat Railway, com vistas espetaculares das montanhas.
+          ➤ Entrada para o Museu Matterhorn, para mergulhar na história e alpinismo da região.
+          ➤ Trilhas guiadas durante o verão ou experiências únicas de esqui durante o inverno.
+
+      Garanta já a sua vaga e venha viver momentos inesquecíveis! ✨
+    ''',
     destinos: ['Zermatt'],
     preco: 4990.00,
     duracaoDias: 10,
